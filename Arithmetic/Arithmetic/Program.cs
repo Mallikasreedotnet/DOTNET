75409d0a851c355ea534fd0fdcfe2363ed7c6106
Mallikasreedotnet <kuntumallamallika64@gmail.com>
2022-06-01 13:15:07 +0530

    Commit

---
+++ Arithmetic/Arithmetic/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ArithmeticOperators;
+{
+    Console.WriteLine("Enter any number");
+    int i = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Enter which arithmetic operator you want : +,-,*,/,% ");
+    char j = Convert.ToChar(Console.ReadLine());
+    Console.WriteLine("Enter any number");
+    int k = Convert.ToInt32(Console.ReadLine());
+    try
+    {
+        Console.WriteLine("Result: " + Operator.Operations(i, j, k));
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message.ToString());
+    }
+}
---
+++ Arithmetic/ArithmeticOperators/Operator.cs
@@ -0,0 +1,45 @@
+﻿namespace ArithmeticOperators
+{
+    public class Operator
+    {
+        public static int Operations(int i, char j, int k)
+        {
+            switch (j)
+            {
+                case '+':
+                    return Addition(i, k);
+                case '-':
+                    return Subtraction(i, k);
+                case '*':
+                    return Multiplication(i, k);
+                case '/':
+                    return Division(i, k);
+                case '%':
+                    return Modulus(i, k);
+                default:
+                    throw new Exception("Enter arithmetic operator only");
+            }
+        }
+        public static int Addition(int a, int b)
+        {
+            return (a + b);
+        }
+        public static int Subtraction(int a, int b)
+        {
+            return (a - b);
+        }
+        public static int Multiplication(int a, int b)
+        {
+            return (a * b);
+        }
+        public static int Division(int a, int b)
+        {
+            return (a / b);
+        }
+        public static int Modulus(int a, int b)
+        {
+            return (a % b);
+        }
+
+    }
+}
