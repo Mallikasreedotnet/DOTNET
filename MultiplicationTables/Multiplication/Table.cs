c22dd3e8aa271f7ac7295bc91912eae4c33216ca
Mallikasreedotnet <kuntumallamallika64@gmail.com>
2022-06-01 14:54:50 +0530

    Commit

---
+++ MultiplicationTables/Multiplication/Table.cs
@@ -0,0 +1,17 @@
+﻿namespace Multiplication
+{
+    public class Table
+    {
+       
+
+        public static void Multiplication(int num)
+        {
+            int i;
+            
+            for (i = 1; i <= 20; i++)
+            {
+               Console.WriteLine($"{num} * {i} = {num*i}");
+            }
+        }
+    }
+}
---
+++ MultiplicationTables/MultiplicationTables/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Multiplication;
+Console.WriteLine("Enter any number which table you want ");
+int num = Convert.ToInt32(Console.ReadLine());
+Table.Multiplication(num);
