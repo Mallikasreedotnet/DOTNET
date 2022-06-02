0c3946f5a20160bf01c0a062ff826ce200f685cf
Mallikasreedotnet <kuntumallamallika64@gmail.com>
2022-06-02 18:00:37 +0530

    Commit

---
+++ UpperandLowerCaseAlphabets/ClassUpperandLowerCase/UpperandLower.cs
@@ -0,0 +1,22 @@
+﻿namespace ClassUpperandLowerCase
+{
+    public class UpperandLower
+    {
+      public static void Alphabets(char ch)
+        {
+           
+            if(ch>= 'a' && ch<='z')
+            {
+              Console.WriteLine($"This is lowercase alphabet : {ch}");
+            }
+            else if(ch>= 'A' && ch<='Z')
+            {
+                Console.WriteLine($"This is uppercase alphabet : {ch}");
+            }
+            else
+            {
+                Console.WriteLine($"This not a alphabet : {ch}");
+            }
+        }
+    }
+}
---
+++ UpperandLowerCaseAlphabets/UpperandLowerCaseAlphabets/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using ClassUpperandLowerCase;
+Console.WriteLine("Enter any character");
+char ch=Convert.ToChar(Console.ReadLine());
+UpperandLower.Alphabets(ch);
+
