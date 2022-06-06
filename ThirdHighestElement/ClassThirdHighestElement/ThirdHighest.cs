4f578fca13f44736a55dc699da34075772ce0a59
Mallikasreedotnet <kuntumallamallika64@gmail.com>
2022-06-06 14:42:29 +0530

    commit

---
+++ ThirdHighestElement/ClassThirdHighestElement/ThirdHighest.cs
@@ -0,0 +1,37 @@
+﻿namespace ClassThirdHighestElement
+{
+    public class ThirdHighest
+    {
+      public static void Elements()
+        {
+            int  i, first=0,second=0, third=0;
+            int[] arr = new int[50];
+           int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Enter elements in the array : {num}");
+            //Console.WriteLine($"Enter {0} elements in the array : {num}");
+            for (i = 0; i < num; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            
+            if(arr[i] > first)
+                {
+                    third = second;
+                    second = first;
+                    first = arr[i];
+                }
+            else if (arr[i]>second)
+                {
+                    third = second;
+                    second=arr[i];
+                }
+            else if (arr[i]>third)
+                {
+                    third=arr[i];
+                }
+          
+            }
+            Console.WriteLine($"This is the third highest number in the array : {third}");
+        }
+
+    }
+}
---
+++ ThirdHighestElement/ThirdHighestElement/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ClassThirdHighestElement;
+Console.WriteLine("Enter the size of array");
+//int num=Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine($"Enter {0} elements in the array : {num}");
+ThirdHighest.Elements();
