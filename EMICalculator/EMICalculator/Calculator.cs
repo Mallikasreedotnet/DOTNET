9765d9082023908eaf7a63e7509cd6d065a051a0
Mallikasreedotnet <kuntumallamallika64@gmail.com>
2022-06-16 11:40:49 +0530

    Commited

---
+++ EMICalculator/EMICalculator/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMICalculator
+{
+    public class Calculator
+    {
+        public static void Calculation(long Amount)
+        {
+            float Percentage = (Amount / 100)*5;
+            for (int i = 1; i <= 12; i++)
+            {
+                Console.WriteLine($"{i}  month You Pay Amount is  : {(Amount + (Percentage * i)) / i}");
+            }
+        }
+        public static long MonthlyEmi(long Amount, int Months)
+        {
+            long percentage = (Amount / 100) * 5;
+            long Totalamount = Amount + (percentage * Months);
+            long Monthpay = Totalamount / Months;
+            return Monthpay;
+            }
+
+        }
+}
---
+++ EMICalculator/EMICalculator/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using EMICalculator;
+Console.Write("Enter The Loan Amount : ");
+long Amount = Convert.ToInt32(Console.ReadLine());
+Calculator.Calculation(Amount);
+Console.Write("Enter How many months of emi : ");
+int Months = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"You Pay Every Month Amount is : {Calculator.MonthlyEmi(Amount , Months)}  ");
