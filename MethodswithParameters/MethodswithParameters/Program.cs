16110e6fa6567aae1957e76f0d853c180fffb44f
Mallikasreedotnet <106054707+Mallikasreedotnet@users.noreply.github.com>
2022-07-17 15:02:54 +0530

    Delete MethodswithParameters directory

--- MethodswithParameters/MethodswithParameters/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-//Console.WriteLine("Hello, World!");
-using MethodswithParameters;
-int i = 40;
-ClassMethods.Methods(20,50);
-Console.ReadLine();
-
